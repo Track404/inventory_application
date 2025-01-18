751e1598f19d5e44fa346c374e63a1b928c18d75
Track404 <111713000+Track404@users.noreply.github.com>
2025-01-18 11:32:56 +0100

    validation and sanitize the user input

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,5 +1,39 @@
 const db = require('../db/query');
 
+const { body, validationResult } = require('express-validator');
+
+// validation of the input data
+const alphaErr = 'must only contain letters.';
+const lengthErr = 'must be between 1 and 20 characters.';
+const yearErr = 'must be between 1800 and 2100';
+const minutesErr = 'must be between 1min and 1000min';
+
+const validateUser = [
+  body('title')
+    .trim()
+
+    .isLength({ min: 1, max: 20 })
+    .withMessage(`Movie ${lengthErr}`),
+  body('director').trim().notEmpty(),
+  body('year')
+    .trim()
+
+    .isInt({
+      min: 1800,
+      max: 2100,
+    })
+    .withMessage(`Year ${yearErr}`),
+
+  body('number_of_minutes')
+    .trim()
+
+    .isInt({
+      min: 1,
+      max: 1000,
+    })
+    .withMessage(`Number of minutes ${minutesErr}`),
+];
+
 async function getMovies(req, res) {
   const movies = await db.getAllMovies();
   res.render('item', { title: 'Show Movies', movies: movies });
@@ -12,6 +46,13 @@ async function createMovie(req, res) {
 
 async function createMoviePost(req, res) {
   const { title, director, year, number_of_minutes } = req.body;
+  const dbDirector = await db.getAllDirectors();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .render('createMovie', { directors: dbDirector, errors: errors.array() });
+  }
   await db.insertMovie(
     title,
     Number(director),
@@ -29,6 +70,16 @@ async function updateMovie(req, res) {
 
 async function updateMoviePost(req, res) {
   const { title, director, year, number_of_minutes } = req.body;
+  const movie = await db.getSingleMovie(req.params.title);
+  const dbDirector = await db.getAllDirectors();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('updateMovie', {
+      movies: movie,
+      directors: dbDirector,
+      errors: errors.array(),
+    });
+  }
   await db.updateMovie(
     title,
     director,
@@ -46,6 +97,7 @@ async function deleteMoviePost(req, res) {
 }
 
 module.exports = {
+  validateUser,
   getMovies,
   createMovie,
   createMoviePost,
--- routes/dbroute.js
+++ routes/dbroute.js
@@ -5,8 +5,12 @@ const dbRouter = Router();
 
 dbRouter.get('/', dbController.getMovies);
 dbRouter.get('/new', dbController.createMovie);
-dbRouter.post('/new', dbController.createMoviePost);
+dbRouter.post('/new', dbController.validateUser, dbController.createMoviePost);
 dbRouter.get('/:title/update', dbController.updateMovie);
-dbRouter.post('/:title/update', dbController.updateMoviePost);
+dbRouter.post(
+  '/:title/update',
+  dbController.validateUser,
+  dbController.updateMoviePost
+);
 dbRouter.post('/:title/delete', dbController.deleteMoviePost);
 module.exports = dbRouter;
